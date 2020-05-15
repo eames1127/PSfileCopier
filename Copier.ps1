750f8e3691150e990fd60126f37c2333fd3cb76d
eames1127 <daniel.eames@googlemail.com>
2020-05-15 19:44:07 +0100

    Text colors added.

--- Copier.ps1
+++ Copier.ps1
@@ -11,17 +11,17 @@ if($copyFrom -eq "")
     $copyFrom = Get-Location
 }
 
-Write-Host "Copy from location" $copyFrom
-Write-Host "Copy to location" $copyTo
+Write-Host "Copy from location" $copyFrom -ForegroundColor DarkYellow
+Write-Host "Copy to location" $copyTo -ForegroundColor DarkYellow
 
 if($extFilter -eq "")
 {
-    Write-Host "No file extension(s) have been specified, are you sure you want to continue? y/n"
+    Write-Host "No file extension(s) have been specified, are you sure you want to continue? y/n" -ForegroundColor DarkYellow
     $continue = Read-Host
 }
 if(-not($continue.ToLower() -eq 'y'))
 {
-    Write-Host "Aborted"
+    Write-Host "Aborted" -ForegroundColor Red
 }
 else {    
     foreach ($file in Get-ChildItem -Path $copyFrom "\*" -Recurse -Include $extFilter)
@@ -29,8 +29,8 @@ else {
         Write-Host "Copying" $file "to" $copyTo
         Copy-Item $file -Destination $copyTo -Confirm
     }
-    Write-Host "Finished copying files to" $copyTo
+    Write-Host "Finished copying files to" $copyTo -ForegroundColor DarkYellow
 }
-Write-Host "Script complete."
+Write-Host "Script complete." -ForegroundColor Green
 
 pause
